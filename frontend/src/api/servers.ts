b026e4a2c9d0f438f135d387ca12c841e56a359b
TenViki <viktorkomarek28@gmail.com>
2022-08-16 18:49:49 +0200

    :fire: Removes webhook shit from frontend

--- frontend/src/api/servers.ts
+++ frontend/src/api/servers.ts
@@ -1,6 +1,5 @@
-import { GuildBasedChannel } from "discord.js";
 import { server } from "../config/backend";
-import { UserGuilds } from "../types/discord";
+import { DiscordChannel, UserGuilds } from "../types/discord";
 
 export const getGuilds = (token: string) => {
   return server.get<UserGuilds[] & { bot: boolean }>("/discord/guilds", {
@@ -12,41 +11,11 @@ export const getGuilds = (token: string) => {
 
 export const getChannels = (token: string, guildId: string) => {
   return server.get<{
-    channels: (GuildBasedChannel & {
-      canSendMessages: boolean;
-      webhook?: {
-        guild: string;
-        channel: string;
-        _id: string;
-      };
-    })[];
+    channels: DiscordChannel[];
     guild: UserGuilds;
   }>(`/discord/guilds/${guildId}`, {
     headers: {
       Authorization: token,
     },
   });
-};
-
-export const createWebhook = (options: {
-  token: string;
-  guildId: string;
-  channelId: string;
-}) => {
-  return server.post<{
-    guild: string;
-    channel: string;
-    _id: string;
-  }>(
-    `/discord/guilds/${options.guildId}/${options.channelId}`,
-    {
-      guild: options.guildId,
-      channel: options.channelId,
-    },
-    {
-      headers: {
-        Authorization: options.token,
-      },
-    }
-  );
 };
